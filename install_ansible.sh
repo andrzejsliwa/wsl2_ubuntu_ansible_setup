86b49f6905577ab13892b5e1830430f1edbfa0ac
Fabrice Semti <fabrice@fabricesemti.com>
2023-12-03 12:55:52 +0000

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -8,3 +8,8 @@ sudo apt update
 sudo apt install -y aptitude ansible dialog
 sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq &&\
     sudo chmod +x /usr/bin/yq
+    
+    
+ansible-galaxy collection install bec.shell --force
+ansible-galaxy role install gantsign.oh-my-zsh --force
+ansible-galaxy role install dotstrap.fzf --force
