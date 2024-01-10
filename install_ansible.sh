946ad554ea7cb85bc6f8f4a49df3b1e34c000ff3
Fabrice Semti <fabrice@fabricesemti.com>
2024-01-10 13:18:03 +0000

    feat: 🚀 install nix package manager
    - refs:

--- install_ansible.sh
+++ install_ansible.sh
@@ -7,9 +7,10 @@ sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt update
 sudo apt install -y aptitude ansible dialog
 sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq &&\
-    sudo chmod +x /usr/bin/yq
-    
+    sudo chmod +x /usr/bin/yq    
     
 ansible-galaxy collection install bec.shell --force
 ansible-galaxy role install gantsign.oh-my-zsh --force
+
+ansible-galaxy role install danielrolls.nix --force
 ansible-galaxy role install dotstrap.fzf --force
