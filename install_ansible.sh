d9d975bf568a8f608a5b14b34acd03e46d088efd
Fabrice Semti <fabrice@fabricesemti.com>
2024-01-12 09:29:29 +0000

    refactor: 💄 use pre-created role to install Docker for better results
    https://galaxy.ansible.com/ui/standalone/roles/geerlingguy/docker/documentation/

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-# install ansible ppa
+# add Ansible repo
 sudo apt-add-repository ppa:ansible/ansible -y
 
-# install stuff
+# Install pre-requisites
 sudo apt update
 sudo apt install -y aptitude ansible dialog
 sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq &&\
     sudo chmod +x /usr/bin/yq    
-    
-ansible-galaxy collection install bec.shell --force
-ansible-galaxy role install gantsign.oh-my-zsh --force
 
-ansible-galaxy role install danielrolls.nix --force
-ansible-galaxy role install dotstrap.fzf --force
+# Install used collections    
+ansible-galaxy collection install bec.shell --force
+
+# Install used roles
+ansible-galaxy role install danielrolls.nix  --force
+ansible-galaxy role install dotstrap.fzf  --force
+ansible-galaxy role install gantsign.oh-my-zsh  --force
+ansible-galaxy role install geerlingguy.docker --force
+ansible-galaxy role install geerlingguy.pip --force
+
